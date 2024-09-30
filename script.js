c199e0af960e00a3fe63d5431b890a340af71e5c
Umesh Deogharia <143274287+Umesh-Deogharia@users.noreply.github.com>
2024-09-30 21:17:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+let root = document.queryselector('html');
+let level = document.getElementById('level');
+let parent = document.documentElement;
+let count = 0;
+while (level !== parent) {
+    count++;
+    level = level.parentNode;
+}
+
+alert(`The level of the element is: ${count}`);
